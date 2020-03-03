c801537a0dce871fd19889da874abf1193a317db
Blackstar699 <morel.matthieu@univ-catholyon.fr>
2020-03-03 15:39:52 +0100

    1ere version develop

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ print("ma mère c'est la meilleure")
 print("mon père c'est la basegit")
 print("bonjour à tousgi")
 print("Voici ma version")
-red(rd(ed))
+# red(rd(ed))
 
 print("c'est matt")
+print("0.1")
